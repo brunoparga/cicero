e067e3bba9f6c64aad5418791a0d4e584b0f3d44
Bruno Parga <brunoparga@gmail.com>
2020-04-15 17:21:18 -0400

    Ensure at most one button per row is selected

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,19 +4,13 @@ import './Button.css';
 
 type Props = {
   content: string;
+  clicked: boolean
 }
 
-const Button: React.FunctionComponent<Props> = ({ content }) => {
-  const [clicked, setClicked] = React.useState(false);
-  return (
-    <button
-      type="button"
-      className={`item btn ${clicked ? 'active' : 'inactive'}`}
-      onClick={() => setClicked(!clicked)}
-    >
-      {content}
-    </button>
-  );
-};
+const Button: React.FunctionComponent<Props> = ({ content, clicked }) => (
+  <button type="button" className={`item btn ${clicked ? 'active' : 'inactive'}`}>
+    {content}
+  </button>
+);
 
 export default Button;
--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
 
 import Button from './Button';
@@ -8,13 +10,28 @@ type Props = {
   items: string[];
 }
 
-const Buttons: React.FunctionComponent<Props> = ({ label, items }) => (
-  <div className="buttons">
-    <div className="item label">
-      {label}
+const Buttons: React.FunctionComponent<Props> = ({ label, items }) => {
+  const [selected, setSelected] = React.useState('');
+
+  // Ensure at most one button per row is selected
+  const clickHandler = (event: React.MouseEvent) => {
+    const elt = event.target as HTMLElement;
+    if (elt.tagName === 'BUTTON' && elt.innerText === selected) {
+      // Clicking an already selected button deselects it
+      setSelected('');
+    } else if (elt.tagName === 'BUTTON') {
+      setSelected(elt.innerText);
+    }
+  };
+
+  return (
+    <div className="buttons" onClick={clickHandler}>
+      <div className="item label">
+        {label}
+      </div>
+      {items.map((item) => <Button content={item} key={item} clicked={item === selected} />)}
     </div>
-    {items.map((item) => <Button content={item} key={item} />)}
-  </div>
-);
+  );
+};
 
 export default Buttons;
