df0fb43988adf9b90b87e2e0f8d0b83f80eef975
Bruno Parga <brunoparga@gmail.com>
2020-07-16 18:22:42 +0300

    Set 'Etcetera' questionType for the relevant words

--- src/store/actions/fetchWords.ts
+++ src/store/actions/fetchWords.ts
@@ -1,11 +1,18 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
+import { QuestionType, Word } from '../../types/word';
 import { Action } from '../../types';
 import { actionTypes } from '../reducer';
 
 export const fetchWords = (dispatch: React.Dispatch<Action>) => {
   fetch('http://localhost:3001/words/')
     .then((res) => res.json())
-    .then((payload) => {
+    .then((words) => {
+      const payload = words.map((word: Word): Word => {
+        if (['Adjective', 'Noun', 'Verb'].includes(word.questionType)) {
+          return word;
+        }
+        return { ...word, questionType: QuestionType.Etcetera };
+      });
       dispatch({ type: actionTypes.SET_WORDS, payload });
     })
     .then(() => dispatch({ type: actionTypes.SET_WORD }));
