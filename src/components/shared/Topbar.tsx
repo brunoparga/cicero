82f988a2bfe36d7727142f2b903238cef73530a2
Bruno Parga <brunoparga@gmail.com>
2020-09-15 20:42:38 +0300

    Improve form with react-hook-form package

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import './Topbar.css';
+
+type Inputs = {
+  email: string,
+  password: string,
+};
 
 export const Topbar: React.FunctionComponent = () => {
   const [action] = React.useState('signin');
+  const URL = `${process.env.REACT_APP_API_URL}/${action}`;
+  const { register, handleSubmit, errors } = useForm<Inputs>();
+
+  const onSubmit = (formData: any) => {
+    fetch(URL, {
+      method: 'POST',
+      headers: { ContentType: 'application/json' },
+      body: JSON.stringify(formData),
+    });
+  };
   return (
     <div className="navbar">
       <div className="title">Cicero</div>
-      <form action={`${process.env.REACT_APP_API_URL}/${action}`} method="post">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" name="email" placeholder="Email" ref={register({ required: true })} />
+        {errors.email && <span>This field is required</span>}
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          ref={register({ required: true })}
+        />
+        {errors.password && <span>This field is required</span>}
         <button type="submit" className="navbar-btn">Sign in</button>
       </form>
     </div>
