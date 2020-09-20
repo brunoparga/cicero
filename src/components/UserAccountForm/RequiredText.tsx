f27a1f8334817d6c6c41c8dabdbd764f966f1a7b
Bruno Parga <brunoparga@gmail.com>
2020-09-20 16:33:39 +0300

    Complete the user account UI

--- src/components/UserAccountForm/RequiredText.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { FormInputPropTypes } from '../../types';
-import { RequiredInput } from './RequiredInput';
-
-export const RequiredText: React.FunctionComponent<FormInputPropTypes> = (props) => React
-  .createElement(RequiredInput, { ...props, type: 'text' });
