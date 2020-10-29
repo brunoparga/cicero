d0eae60f0114a652ff84b788a4677f4a01689511
Bruno Parga <brunoparga@gmail.com>
2020-10-29 15:59:00 +0300

    Improve the look of the front page

--- src/components/FrontPage.tsx
+++ src/components/FrontPage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { actionTypes, fetchWords, store } from '../store';
+import './FrontPage.css';
+import './shared/Button.css';
 
 export const FrontPage: React.FunctionComponent = () => {
   const { dispatch } = React.useContext(store);
@@ -7,5 +9,20 @@ export const FrontPage: React.FunctionComponent = () => {
     fetchWords(dispatch);
     dispatch({ type: actionTypes.TOGGLE_STUDYING });
   };
-  return <button type="button" onClick={clickHandler}>STUDY</button>;
+  return (
+    <div className="front-page-container">
+      <h4>Welcome to Cicero!</h4>
+      <p>
+        This is a Latin language learning app. It is based on the list of the ~1500 most common
+        words found in Classical Latin texts.
+      </p>
+      <p>
+        As you learn, Cicero will remember which words you&apos;ve already studied and suggest new
+        ones for your review.
+      </p>
+      <button type="button" className="item btn answer-btn white active big" onClick={clickHandler}>
+        STUDY
+      </button>
+    </div>
+  );
 };
