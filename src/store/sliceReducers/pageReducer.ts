c391bcaf85348e66ba68570e950737d80bbab87f
Bruno Parga <brunoparga@gmail.com>
2020-11-04 12:22:59 +0300

    Move a check from store to reducer
    Previously, the store itself checked whether the user has guessed all of
    the parts of the word. Now, that responsibility is where it belongs, in
     the reducer.

--- src/store/sliceReducers/pageReducer.ts
+++ src/store/sliceReducers/pageReducer.ts
@@ -7,11 +7,16 @@ export const pageReducer = (page: PageState, { type, payload }: Action): PageSta
       return { ...page, currentWordIndex: -1 };
     case actions.RESULTS_SAVED:
       return { ...page, resultsSaved: true };
-    case actions.REVEAL_ANSWER:
-      return { ...page, revealAnswer: true };
     case actions.SET_GUESS: {
       const { guess } = page;
       guess[payload.property] = payload.value;
+      // If the current guess is correct, check if all guesses are correct and reveal answer if so
+      if (payload.value) {
+        const wordIsGuessed = Object.values(page.guess).every((entry: boolean) => entry);
+        if (wordIsGuessed) {
+          return { ...page, guess, revealAnswer: true };
+        }
+      }
       return { ...page, guess };
     }
     case actions.SET_STATUS:
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Action, AppState, AppStateWithDispatch } from '../types';
-import { actions, initialAppState, rootReducer } from '.';
+import { initialAppState, rootReducer } from '.';
 
 const store = React
   .createContext<AppStateWithDispatch>({ state: initialAppState, dispatch: () => {} });
@@ -9,13 +9,6 @@ const store = React
 const StateProvider: React.FunctionComponent = ({ children }) => {
   const [state, dispatch]: [AppState, React.Dispatch<Action>] = React
     .useReducer(rootReducer, initialAppState);
-
-  // Test if all values have been guessed correctly (there has to be a word set)
-  const correctGuess = Object.values(state.page.guess).every((entry: boolean) => entry);
-  if (state.word && correctGuess && !state.page.revealAnswer) {
-    dispatch({ type: actions.REVEAL_ANSWER });
-  }
-
   return <store.Provider value={{ state, dispatch }}>{children}</store.Provider>;
 };
 
