a984a4b71cb85c11dfbeba2d4d178d4db00b9c2d
Bruno Parga <brunoparga@gmail.com>
2020-11-16 19:48:45 +0300

    Send user token back with session results

--- src/store/actionCreators/endStudySession.ts
+++ src/store/actionCreators/endStudySession.ts
@@ -6,11 +6,14 @@ import { actions } from '..';
 export const endStudySession = (words: Word[], dispatch: React.Dispatch<Action>): void => {
   // Using this action at the last word changes the page state to 'done'
   dispatch(actions.SET_WORD);
-  // Record the results in the DB
+  // Record the results in the DB, including the token
   const body = JSON.stringify(words.map((word) => ({ lemma: word.lemma, learned: word.learned })));
   fetch(`${process.env.REACT_APP_API_URL}/words`, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+      'Content-Type': 'application/json',
+    },
     body,
   })
     .then(() => dispatch(actions.RESULTS_SAVED));
