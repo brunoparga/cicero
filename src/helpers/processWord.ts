8ef0798c43c749749b74b4ef9feb25332cc30efd
Bruno Parga <brunoparga@gmail.com>
2020-11-19 18:37:32 +0300

    Improve processWord
    The probability of setting a word to translation was changed to 1/3;
    furthermore, an issue is fixed with indeclinable words with properties,
    such as egō.

--- src/helpers/processWord.ts
+++ src/helpers/processWord.ts
@@ -3,13 +3,13 @@ import { QuestionType, Word } from '../types';
 export const processWord = (word: Word): Word => {
   // IDEA: write question type in the back-end (pro: easy on the front, con:
   // hard with teach and translate)
-  if (word.learned && Math.random() < 0.3) {
+  if (word.learned && Math.random() < (1 / 3)) {
     return { ...word, questionType: QuestionType.Translation };
   } if (word.properties?.indeclinable) {
     return { ...word, questionType: QuestionType.Indeclinable };
   } if (['Adjective', 'Noun', 'Verb'].includes(word.questionType)) {
     return word;
-  } if (['Numeral', 'Pronoun'].includes(word.questionType) && word.properties) {
+  } if (['Numeral', 'Pronoun'].includes(word.questionType) && word.properties.suffixes) {
     return { ...word, questionType: QuestionType.Adjective };
   }
   return { ...word, questionType: QuestionType.Indeclinable };
