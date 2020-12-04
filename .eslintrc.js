2bd290a19b4e6185d2769ec0994a8fd83cb61ecc
Bruno Parga <brunoparga@gmail.com>
2020-12-04 19:29:59 +0300

    Add a few eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,16 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    'jest/globals': true
   },
   extends: [
+    'airbnb',
     'eslint:recommended',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'plugin:jsx-a11y/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-    'airbnb',
   ],
   globals: {
     Atomics: 'readonly',
@@ -22,15 +26,26 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: [
+    'inclusive-language',
+    'jest',
+    'jsx-a11y',
     'react',
     '@typescript-eslint',
+    'woke',
   ],
   rules: {
+    'inclusive-language/use-inclusive-words': 'error',
     'import/extensions': 'off',
     'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
     'no-use-before-define': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
     'react/prop-types': 'off',
+    'woke/all': 'error'
   },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
 };
--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { App } from './App';
 
-test('renders learn react link', () => {
+test('renders learn Latin text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Latin language learning/i);
   expect(linkElement).toBeInTheDocument();
 });
