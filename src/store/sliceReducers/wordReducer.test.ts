a2d4ddceb23af102c86c5a63cac3f5ba8e5e066a
Bruno Parga <brunoparga@gmail.com>
2020-12-10 15:54:48 +0300

    Test slice reducers

---
+++ src/store/sliceReducers/wordReducer.test.ts
@@ -0,0 +1,16 @@
+import { Word } from '../../types';
+import { wordReducer } from './wordReducer';
+
+it('sets the word to learned with the correct action', () => {
+  const word = { lemma: 'corge', learned: false } as Word;
+  const action = { type: 'LEARN_WORD' };
+  const expectation = { lemma: 'corge', learned: true } as Word;
+  expect(wordReducer(word, action)).toEqual(expectation);
+});
+
+it('does nothing with any other action', () => {
+  // at the beginning of the session, word is null and user signs in
+  const word = null;
+  const action = { type: 'SIGN_IN' };
+  expect(wordReducer(word, action)).toEqual(word);
+});
---
+++ src/store/sliceReducers/wordsReducer.test.ts
@@ -0,0 +1,15 @@
+import { Word } from '../../types';
+import { wordsReducer } from './wordsReducer';
+
+it('replaces the words array when necessary', () => {
+  const words = [{ lemma: 'foo' }, { lemma: 'bar' }] as Word[];
+  const action = { type: 'FETCH_WORDS', payload: [{ lemma: 'baz' }, { lemma: 'quux' }] };
+  const expectation = [{ lemma: 'baz' }, { lemma: 'quux' }] as Word[];
+  expect(wordsReducer(words, action)).toEqual(expectation);
+});
+
+it('does nothing with any other action', () => {
+  const words = [{ lemma: 'foo' }, { lemma: 'bar' }] as Word[];
+  const action = { type: 'WHATEVER' };
+  expect(wordsReducer(words, action)).toEqual(words);
+});
